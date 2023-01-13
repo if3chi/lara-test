f27da14f69df839c55f8845c1f4c3e17d13ce536
Truth <j.cyxtus@gmail.com>
2023-01-13 10:09:38 +0000

    Added edit button

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,29 +7,38 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            @if (auth()->user()->is_admin)
+                <div class="mb-4">
+                    <x-action-button>New Product</x-action-button>
+                </div>
+            @endif
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                @if (auth()->user()->is_admin)
-                    <a href="#" class="items-center px-4 py-2 mb-4 bg-gray-900">Add New Product</a>
-                @endif
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @forelse ($products as $product)
-                        <table class="w-full border border-collapse border-slate-500">
-                            <thead>
-                                <tr>
-                                    <th class="border border-slate-600">Product</th>
-                                    <th class="border border-slate-600">Price</th>
-                                </tr>
-                            </thead>
+                    <table class="w-full border border-collapse border-slate-500">
+                        <thead>
+                            <tr>
+                                <th class="border border-slate-600">Product</th>
+                                <th class="border border-slate-600">Price</th>
+                                <th class="border border-slate-600">Action</th>
+                            </tr>
+                        </thead>
+                        @forelse ($products as $product)
                             <tbody class="text-center">
                                 <tr>
                                     <td class="border border-slate-700 ...">{{ $product->name }}</td>
                                     <td class="border border-slate-700 ...">{{ $product->price }}</td>
+                                    <td class="border border-slate-700 ...">
+                                        @if (auth()->user()->is_admin)
+                                            <x-action-button href="{{ route('products.edit', $product->id) }}">Edit
+                                            </x-action-button>
+                                        @endif
+                                    </td>
                                 </tr>
                             </tbody>
-                        </table>
-                    @empty
-                        <div class="text-lg text-center">{{ __('Nothing to show') }}</div>
-                    @endforelse
+                        @empty
+                            <div class="text-lg text-center">{{ __('Nothing to show') }}</div>
+                        @endforelse
+                    </table>
                 </div>
             </div>
         </div>
